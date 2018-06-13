e66519dff7170631db02e4ed9b37f965c2ac7c8c
Evan Moon <bboydart91@gmail.com>
2018-06-13 16:44:18 +0900

    Add auto timestamp mixin to User

--- account/models.py
+++ account/models.py
@@ -1,11 +1,20 @@
 from django.utils import timezone
 from django.db import models
+from django.core.mail import send_mail
 from django.contrib.auth.models import (
     AbstractBaseUser, PermissionsMixin
 )
 from .managers import UserManager
+from base.mixins.timestamp import (
+    AutoCreatedUpdatedMixin, SoftDeleteMixin,
+)
 
-class User(AbstractBaseUser):
+class User(
+    AbstractBaseUser,
+    PermissionsMixin,
+    AutoCreatedUpdatedMixin,
+    SoftDeleteMixin
+):
     # _()은 i18n 될 수 있는 함수를 의미
 
     class Meta:
@@ -17,19 +26,28 @@ class User(AbstractBaseUser):
     has_privacy_policy = models.BooleanField(default=False, )
     is_active = models.BooleanField(default=True, )
     is_admin = models.BooleanField(default=False, )
-    created_at = models.DateTimeField(default=timezone.now, )
 
     objects = UserManager()
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'has_terms', 'has_privacy_policy', ]
+
+    def __str__(self):
+        return "username: " + self.username
 
     def get_full_name(self):        
         return self.username
 
     def get_short_name(self):
         return self.username
+
+    def email_user(self, subject, message, from_email=None):
+        send_mail(subject, message, from_email, [self.email])
+        
+    def save(self, *args, **kwargs):
+        return super(User, self).save(*args, **kwargs)
     
     @property
     def is_staff(self):
+        "Is the user a member of staff?"
         return self.is_admin
 
